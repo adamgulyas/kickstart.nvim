8856905529efc65e868e744bbd49448a8d876516
Adam Gulyas <aagulyas@gmail.com>
2024-05-29 19:16:13 -0400

    Add vim-obsession

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -149,7 +149,7 @@ return {
     lazy = false,
   },
   {
-    -- 'github/copilot.vim',
+    'github/copilot.vim',
   },
   {
     'kdheepak/lazygit.nvim',
@@ -171,17 +171,17 @@ return {
     },
   },
   {
-    -- 'CopilotC-Nvim/CopilotChat.nvim',
-    -- branch = 'canary',
-    -- dependencies = {
-    --   { 'github/copilot.vim' }, -- or github/copilot.vim
-    --   { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-    -- },
-    -- opts = {
-    --   debug = true, -- Enable debugging
-    --   -- See Configuration section for rest
-    -- },
-    -- -- See Commands section for default commands if you want to lazy load on them
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
   {
     'kawre/neotab.nvim',
@@ -233,5 +233,8 @@ return {
     config = function()
       require('nvim-highlight-colors').setup {}
     end,
+  },
+  {
+    'tpope/vim-obsession',
   },
 }

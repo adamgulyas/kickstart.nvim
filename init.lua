e625aff7d5e8c9f1034d5052e1045506253b1dd4
Adam Gulyas <adam.gulyas@morningstar.com>
2024-06-12 15:18:56 -0400

    Configure typescript lsp server

--- init.lua
+++ init.lua
@@ -499,6 +499,26 @@ require('lazy').setup({
         filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' }, -- Supported file types
       }
 
+      lspconfig.tsserver.setup {
+        on_attach = function(client, bufnr)
+          local function buf_set_keymap(...)
+            vim.api.nvim_buf_set_keymap(bufnr, ...)
+          end
+          local function buf_set_option(...)
+            vim.api.nvim_buf_set_option(bufnr, ...)
+          end
+
+          -- Enable completion triggered by <c-x><c-o>
+          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- Mappings.
+          local opts = { noremap = true, silent = true }
+
+          -- Disable tsserver's formatting in favor of ESLint
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+      }
+
       -- require('lspconfig').dotenvls.setup {}
 
       -- Emmet Language Server setup

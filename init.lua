61becc7a74a52695dd7e54723b4a19fbd7cc3613
Adam Gulyas <aagulyas@gmail.com>
2024-08-06 00:26:15 -0400

    Whick-key fix (attempt)

--- init.lua
+++ init.lua
@@ -353,26 +353,28 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  { -- Useful plugin to show you pending keybinds.
+  {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+    event = 'vimenter', -- sets the loading event to 'vimenter'
+    config = function() -- this is the function that runs, after loading
+      local wk = require 'which-key'
+      wk.setup()
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
+      -- document existing key chains
+      wk.register({
+        c = { name = '[C]ode' },
+        d = { name = '[D]ocument' },
+        h = { name = 'Git [H]unk' },
+        r = { name = '[R]ename' },
+        s = { name = '[S]earch' },
+        t = { name = '[T]oggle' },
+        w = { name = '[W]orkspace' },
+      }, { prefix = '<leader>' })
+
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      wk.register({
+        h = { name = 'Git [H]unk' },
+      }, { mode = 'v', prefix = '<leader>' })
     end,
   },
 
